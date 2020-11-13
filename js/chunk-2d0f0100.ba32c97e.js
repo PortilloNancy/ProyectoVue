87ab25a10957ae23ccb1359e830605b5956b5ec9
PortilloNancy <portillo.nancy77@hotmail.com>
2020-11-13 19:34:57 -0300

    deploy

---
+++ js/chunk-2d0f0100.ba32c97e.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0f0100"],{"9b89":function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("section",{attrs:{id:"feed"}},[t("feed",[e._t("default")],2)],1)},c=[],d=(t("d3b7"),{name:"HomeAbout",components:{Feed:function(){return t.e("chunk-6d6cf4ba").then(t.bind(null,"f5bd"))}}}),o=d,l=t("2877"),a=Object(l["a"])(o,u,c,!1,null,null,null);n["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d0f0100.ba32c97e.js.map
